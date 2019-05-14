b2c90127c9b074d4c2f6fe0011765e66ad5a5df9
GaruGaru <garuglieritommaso.dev@gmail.com>
2019-05-14 21:09:13 +0200

    Refactor using angular component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { PersonalProjectsComponent } from './personal-projects/personal-projects.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { LanguagesComponent } from './languages/languages.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    WorkExperienceComponent,
+    PersonalProjectsComponent,
+    ContactsComponent,
+    SkillsComponent,
+    EducationComponent,
+    LanguagesComponent,
+    HeaderComponent,
+
   ],
   imports: [
     BrowserModule
